e0a898af160ad078756d44ffb3ff7355ce7ae656
Adam Lipson <alipson98@gmail.com>
2021-04-13 22:52:55 -0400

    modified trow detection code to take a stream of data rather than an array. Still not great

--- throw_detect.py
+++ throw_detect.py
@@ -5,7 +5,7 @@ import statistics
 
 def throw_detect(time, accel_x, accel_y, accel_z, accel_mag):
     """
-    take a stream of acceleration values and identify throws
+    take an array of acceleration values and identify throws
     currently returns nothing, just prints out the samples when the throw occurs
     """
     num_throws = 0
@@ -42,7 +42,8 @@ def throw_detect(time, accel_x, accel_y, accel_z, accel_mag):
                 print("Released at %d" % release_sample) # get release angle here
                 # note: release_sample is the local max of acceleration - need to integrate 3 or 4 samples past to get velocity
                 # also don't start calculating centripetal acceleration until 3 or 4 samples after release_sample
-                print("Finished at %d\n" % end_sample)
+                print("Finished at %d" % end_sample)
+                print("Length: %d\n" % (end_sample - release_sample))
                 # TODO: do something with these numbers
                 num_throws += 1
         else:
@@ -76,7 +77,6 @@ def throw_detect(time, accel_x, accel_y, accel_z, accel_mag):
 
     print("total of %d throws" % num_throws)
 
-
 time = []
 accel_x = []
 accel_y = []
@@ -94,3 +94,109 @@ with open('mult_throw_1.csv') as f:
 
 throw_detect(time, accel_x, accel_y, accel_z, accel_mag)
 
+
+############
+# Note:
+# The best way to handle get_next would probably be with multithreading
+# one thread could pull data off of the imu at the correct times and add it to the back of an array
+# another thread could consume data off the front of that array and do the processing
+# I'm not implementing that right now, but it's an option if we need
+############
+
+# print(type(time[0]))
+# redefine get_next to get the next imu data instead of next array element
+# get_next should contain the sleep
+i = 0
+num_throws = 0
+def get_next():
+    global i
+    i += 1
+    if i >= len(accel_mag):
+        print(str(num_throws)+ " throws")
+        exit(0)
+    # also will need to calculate accel_mag here
+    return accel_mag[i] # currently only using these but may modify to use more
+    
+
+# constants
+avg_lookback = 10
+min_throw_samples = 10
+forward_len = 10
+forward_skip = 3
+
+# thresholds
+max_flight_stdev = 3.5
+release_peak_height = 25
+min_throw_jerk = 1
+throw_end_jerk = 5
+lookback_arr_len = 50
+
+start_sample = 0
+release_sample = 0
+end_sample = 0
+in_flight = False
+
+lookback_arr = []
+
+while len(lookback_arr) < lookback_arr_len:
+        curr_accel_mag = get_next()
+        lookback_arr.append(curr_accel_mag)
+
+while (True): # run forever for now
+    if in_flight:
+        curr_throw_len += 1
+        last_avg = sum(lookback_arr[len(lookback_arr) - avg_lookback - 1 : len(lookback_arr) - 1]) / avg_lookback
+        if (abs(lookback_arr[-1] - last_avg) > throw_end_jerk and curr_throw_len > min_throw_samples):
+            in_flight = False
+            num_throws += 1
+            print("Throw of length %d, end at %d" % (curr_throw_len, i))
+            # could do something else here if we want
+    else:
+        # print("checking "+str(i))
+        next_avg = sum(lookback_arr[len(lookback_arr) - forward_len - forward_skip : len(lookback_arr) - forward_skip]) / forward_len
+        next_stdev = statistics.stdev(lookback_arr[len(lookback_arr) - forward_len : len(lookback_arr) - forward_skip])
+        if (lookback_arr[len(lookback_arr) - forward_len - forward_skip] - next_avg) > release_peak_height and next_stdev < max_flight_stdev:
+            curr_throw_len = 0
+            in_flight = True
+            # released at lookback_arr[len(lookback_arr) - forward_skip - forward_len]
+            found = False
+            idx = len(lookback_arr) - forward_skip - forward_len
+            release_idx = idx
+            found_positive = False
+            invalid = False        
+
+            while not found:
+                curr_jerk = lookback_arr[idx] - lookback_arr[idx - 1]
+                if curr_jerk > 0:
+                    found_positive = True
+                if found_positive and curr_jerk != 0 and curr_jerk < min_throw_jerk:
+                    found = True
+                    start_sample = idx
+                idx -= 1
+                if (idx <= 0): # could not find the release point
+                    invalid = True
+                    start_sample = 0
+                    break
+                
+            # curr_throw_len = forward_len + forward_skip
+            print(release_idx - start_sample)
+            
+            # TODO: here is were to integrate lookback_arr from start_sample to release_idx
+            # calculate release angle at release_idx
+            
+            # pull enough samples into the array
+            # don't need this loop unless avg lookback is larger than forward_len + forward_skip
+            # for dummy in range(avg_lookback - (forward_len + forward_skip)):
+            #     curr_accel_mag = get_next()
+            #     lookback_arr.pop(0)
+            #     lookback_arr.append(curr_accel_mag)
+            
+            # to get centripetal acceleration, we'll need to modify get_next to also return accel_x and accel_y
+            # use curr_accel_x and curr_accel_y to calculate centripetal acceleration of the throw
+
+    next_accel_mag = get_next()
+    lookback_arr.pop(0)
+    lookback_arr.append(next_accel_mag)
+
+
+    
